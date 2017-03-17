5fc12d9dc4a6d3ec07ac6118fdad1a0a84545474
ak1103dev <ak.apichan1103@gmail.com>
2017-03-17 22:32:54 +0700

    remove eslint error

--- apolloserver/app.js
+++ apolloserver/app.js
@@ -7,9 +7,9 @@ const app = express();
 const { apolloExpress, graphiqlExpress } = require('apollo-server');
 const { makeExecutableSchema } = require('graphql-tools');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
